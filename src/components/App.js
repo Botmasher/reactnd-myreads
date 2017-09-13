66ab97dfc6541e27c69c6fd0448f06b0fcd63f38
Botmasher <languagejosh@hotmail.com>
2017-09-13 14:22:14 -0700

    refactor: Break out search screen into a separate test component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
+import Search from './Search';
 // import * as BooksAPI from '../utils/BooksAPI'
 import '../App.css'
-
 /*
   TODO: plans for starters
   - use client model as a starter guide for what it could look like
@@ -46,26 +46,7 @@ class BooksApp extends React.Component {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <Search />
         ) : (
           <div className="list-books">
             <div className="list-books-title">
